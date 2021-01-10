3e9d24327d499a487ed2df04114285b735dfb79d
yaroslavStn <yriksotnikov@gmail.com>
2021-01-10 23:07:58 +0200

    add package

--- src/ArrayPrinter.java
+++ src/ArrayPrinter.java
@@ -1,3 +1,4 @@
+import util.Array;
 
 public class ArrayPrinter {
 
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 
 
+import util.Array;
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
 
         ArrayPrinter arrayPrinter= new ArrayPrinter();
         for (; ; ) {
